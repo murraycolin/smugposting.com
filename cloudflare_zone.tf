bcbb5d0b2098c1081ce2a8c65eea1dba70179606
Colin Murray <github@murraycolin.com>
2019-12-17 02:48:04 -0500

    Added CF zone

---
+++ cloudflare_zone.tf
@@ -0,0 +1,27 @@
+provider "cloudflare" {
+  # email pulled from $CLOUDFLARE_EMAIL
+  # token pulled from $CLOUDFLARE_TOKEN
+}
+
+variable "domain" {
+  default = "smugposting.com"
+}
+
+# resource "cloudflare_zone" "smugposting-com" {
+#  zone       = "smugposting.com"
+#  jump_start = false
+#  plan       = "enterprise"
+#  type       = "full"
+# }
+
+resource "cloudflare_zone_settings_override" "smugposting-com" {
+  name = var.domain
+  settings {
+    always_online            = "off"
+    always_use_https         = "on"
+    tls_1_3                  = "on"
+    automatic_https_rewrites = "on"
+    ssl                      = "full"
+    waf                      = "on"
+  }
+}
